02399b1b7b0eca7a5badcdce5d48bc4ad5c3a84b
Joshua_Macaulay <jmacaulay@hotmail.com.au>
2020-01-09 14:19:17 +0800

    Base Program Created

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Life__the_Universe__and_Everything
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 0;
+
+        }
+    }
+}
